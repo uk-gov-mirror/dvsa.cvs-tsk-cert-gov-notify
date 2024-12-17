93605edbb0a6598e2a61282dafaa9b2305f3210a
Nathan Brown <nathancjb@googlemail.com>
2024-12-17 08:01:13 +0000

    feat(cb2-15352): refactor and add VTP/G 12 (#95)
    * feat(cb2-15352): start refactor
    
    * feat(cb2-15352): biome introduction
    
    * feat(cb2-15352): add adapter and start unit testing
    
    * feat(cb2-15352): complete new test coverage
    
    * feat(cb2-15352): add VTPG 12 emailer
    
    * feat(cb2-15352): refactor setting template id
    
    * feat(cb2-15352): repair DI
    
    * feat(cb2-15352): small bug fixes
    
    * feat(cb2-15352): small bug fixes

--- src/functions/govNotify.ts
+++ src/functions/govNotify.ts
@@ -1,71 +1,41 @@
-/* eslint-disable no-await-in-loop */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable consistent-return */
-/* eslint-disable import/no-unresolved */
 import { S3Client } from '@aws-sdk/client-s3';
-import {
-  Callback, Context, Handler, S3Event,
-  SQSBatchItemFailure, SQSBatchResponse, SQSEvent,
-} from 'aws-lambda';
-// @ts-ignore
-import { NotifyClient } from 'notifications-node-client';
+import { Handler, SQSBatchItemFailure, SQSBatchResponse, SQSEvent } from 'aws-lambda';
+import 'reflect-metadata';
+import Container from 'typedi';
 import { ERRORS } from '../assets/enum';
-import { CertificateDownloadService } from '../services/CertificateDownloadService';
 import { NotificationService } from '../services/NotificationService';
-import { S3BucketService } from '../services/S3BucketService';
-import { Configuration } from '../utils/Configuration';
+import { EmailRequestProcessor } from './EmailRequestProcessor';
 
 /**
  * λ function to process an SQS record and initialise email notifications for generated certificates
  * @param event - SQS event
- * @param context - λ Context
- * @param callback - callback function
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const govNotify: Handler = async (event: SQSEvent, context?: Context, callback?: Callback): Promise<SQSBatchResponse> => {
-  if (!event || !event.Records || !Array.isArray(event.Records) || !event.Records.length) {
-    console.error('ERROR: event is not defined.');
-    throw new Error(ERRORS.EventIsEmpty);
-  }
+const govNotify: Handler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
+	if (!event || !event.Records || !Array.isArray(event.Records) || !event.Records.length) {
+		console.error('ERROR: event is not defined.');
+		throw new Error(ERRORS.EventIsEmpty);
+	}
 
-  const downloadService: CertificateDownloadService = new CertificateDownloadService(new S3BucketService(new S3Client()));
-  const notifyConfig = await Configuration.getInstance().getNotifyConfig();
-  const notifyClient = new NotifyClient(notifyConfig.api_key);
-  const notifyService: NotificationService = new NotificationService(notifyClient);
-  const batchItemFailures: SQSBatchItemFailure[] = [];
+	Container.set(S3Client, new S3Client());
+	const processRequest = Container.get(EmailRequestProcessor);
+	const notificationService = Container.get(NotificationService);
+	await notificationService.initializeNotifyClient();
 
-  for (const sqsRecord of event.Records) {
-    try {
-      const objectPutEvent: S3Event = JSON.parse(sqsRecord.body);
+	const batchItemFailures: SQSBatchItemFailure[] = [];
 
-      if (objectPutEvent.Records) {
-        for (const s3Record of objectPutEvent.Records) {
-          const s3Object: any = s3Record.s3.object;
-          // Object key may have spaces or unicode non-ASCII characters.
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-          const decodedS3Key = decodeURIComponent(s3Object.key.replace(/\+/g, ' '));
-          const notifyPartialParams = await downloadService.getCertificate(decodedS3Key);
+	for (const sqsRecord of event.Records) {
+		try {
+			const s3Records = processRequest.getRecordS3Objects(sqsRecord);
+			for (const record of s3Records) {
+				await processRequest.process(record);
+			}
+		} catch (error) {
+			console.error(error);
+			batchItemFailures.push({ itemIdentifier: sqsRecord.messageId });
+		}
+	}
 
-          if (!notifyPartialParams.shouldEmail || notifyPartialParams.shouldEmail === 'true') {
-            if (decodedS3Key.includes('VOSA')) {
-              const emailList = process.env.TFL_EMAIL_LIST?.split(',') ?? [];
-              for (const email of emailList) {
-                notifyPartialParams.email = email;
-                await notifyService.sendNotification(notifyPartialParams);
-              }
-            } else {
-              await notifyService.sendNotification(notifyPartialParams);
-            }
-          }
-        }
-      }
-    } catch (error) {
-      console.error(error);
-      batchItemFailures.push({ itemIdentifier: sqsRecord.messageId });
-    }
-  }
-
-  return { batchItemFailures };
+	return { batchItemFailures };
 };
 
 export { govNotify };
--- src/services/NotificationService.ts
+++ src/services/NotificationService.ts
@@ -1,38 +1,52 @@
 // @ts-ignore
 import { NotifyClient } from 'notifications-node-client';
-import { DocumentTypes, IPartialParams } from '../models';
+import { Service } from 'typedi';
+import { IPartialParams } from '../models';
 import { Configuration } from '../utils/Configuration';
 
 /**
  * Service class for Certificate Notifications
  */
+@Service()
 class NotificationService {
-  private readonly notifyClient: NotifyClient;
+	private notifyClient: NotifyClient;
 
-  private readonly config: Configuration;
+	private readonly config: Configuration;
 
-  constructor(notifyClient: NotifyClient) {
-    this.notifyClient = notifyClient;
-    this.config = Configuration.getInstance();
-  }
+	constructor() {
+		this.config = Configuration.getInstance();
+	}
 
-  /**
-   * Sending email with the certificate according to the given params
-   * @param params - personalization details,email and certificate
-   */
-  public async sendNotification(notifyPartialParams: IPartialParams) {
-    const emailDetails = {
-      personalisation: {
-        ...notifyPartialParams.personalisation,
-        link_to_document: this.notifyClient.prepareUpload(notifyPartialParams.fileData, { confirmEmailBeforeDownload: false, isCsv: notifyPartialParams.documentType === DocumentTypes.TFL_FEED }),
-      },
-    };
-    const templateId = await this.config.getTemplateIdFromEV(notifyPartialParams.documentType);
+	public async initializeNotifyClient() {
+		const notifyConfig = await this.config.getNotifyConfig();
+		this.notifyClient = new NotifyClient(notifyConfig.api_key);
+	}
 
-    console.log(`Personalisation params: ${JSON.stringify(notifyPartialParams.personalisation)} + email ${notifyPartialParams.email}`);
-    await this.notifyClient.sendEmail(templateId, notifyPartialParams.email, emailDetails);
-    console.log(`Sent email using ${templateId} templateId, ${notifyPartialParams.documentType} with ${notifyPartialParams.personalisation.date_of_issue} date of issue`);
-  }
+	/**
+	 * Sending email with the certificate according to the given params
+	 * @param params - personalization details,email and certificate
+	 */
+	public async sendNotification(notifyPartialParams: IPartialParams, templateId: string, isCsv = false) {
+		if (!notifyPartialParams?.shouldEmail || notifyPartialParams?.shouldEmail === 'true') {
+			const emailDetails = {
+				personalisation: {
+					...notifyPartialParams.personalisation,
+					link_to_document: this.notifyClient.prepareUpload(notifyPartialParams.fileData, {
+						confirmEmailBeforeDownload: false,
+						isCsv,
+					}),
+				},
+			};
+
+			console.log(
+				`Personalisation params: ${JSON.stringify(notifyPartialParams.personalisation)} + email ${notifyPartialParams.email}`
+			);
+			await this.notifyClient.sendEmail(templateId, notifyPartialParams.email, emailDetails);
+			console.log(
+				`Sent email using ${templateId} templateId, ${notifyPartialParams.documentType} with ${notifyPartialParams.personalisation.date_of_issue} date of issue`
+			);
+		}
+	}
 }
 
 export { NotificationService };
